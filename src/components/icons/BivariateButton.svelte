e66cba7af7500d35ab791e4f0f61f8f01a1ac2e1
admation <admation60@gmail.com>
2023-04-05 15:19:22 -0400

    polish Q+C bivariate flow

--- src/components/icons/BivariateButton.svelte
+++ src/components/icons/BivariateButton.svelte
@@ -14,25 +14,3 @@
 >
 <path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" d="M10 3a7 7 0 100 14 7 7 0 000-14zm-9 7a9 9 0 1118 0 9 9 0 01-18 0zm14 .069a1 1 0 01-1 1h-2.931V14a1 1 0 11-2 0v-2.931H6a1 1 0 110-2h3.069V6a1 1 0 112 0v3.069H14a1 1 0 011 1z"/>
 </svg>
-
-<svg
-    viewBox="0 0 1024 1024"
-    xmlns="http://www.w3.org/2000/svg"
-    height={size}
-    width={size}
-    fill={color}
-    ><path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M168.96 430.08h128v327.68H168.96zM737.28 363.52h128v394.24h-128zM74.24 829.44h870.4v58.88h-870.4zM547.84 276.48h128v481.28h-128zM358.4 112.64h128v645.12H358.4z"
-    /><path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M67.7376 493.7216l196.5824-196.58752 39.82336 39.82336-196.5824 196.5824zM699.4944 461.39904l209.98144-209.98144 39.82336 39.82336-209.98144 209.98144z"
-    /><path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        a
-        d="M247.91552 351.91296l16.53248-53.84192 492.89728 151.3216-16.53248 53.84192z"
-    /></svg
->
